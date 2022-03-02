7535b1c73a6e56c470b4f655b9dc8fb1ae15c00e
CamelAbasha <100474748+CamelAbasha@users.noreply.github.com>
2022-03-02 08:10:13 +0300

    Update 01_hw.py

--- Module2/home_work/01_hw.py
+++ Module2/home_work/01_hw.py
@@ -6,3 +6,11 @@
 # Выведите «YES», если можно отломить от шоколадки ровно k долек, и «NO» иначе.
 
 # TODO: your code here
+k = int(input("Число долек:"))
+n = int(input("Ширина шоколадки:"))
+m = int(input("Длина шоколадки:"))
+
+if k<n*m and (k%n==0 or k%m==0):
+  print("YES")
+else:
+  print("NO")
