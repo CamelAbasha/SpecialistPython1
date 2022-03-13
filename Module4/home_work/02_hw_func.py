97aaf16599b564b4cac02ba11b1810ca2c156570
CamelAbasha <100474748+CamelAbasha@users.noreply.github.com>
2022-03-13 11:09:19 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -6,8 +6,20 @@
 
 def distance(x1, y1, x2, y2):
     # TODO: тело, которое вы реализовали на практической работе
-    pass
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 
 # TODO: your code here
+point_a, point_b, point_c = (0, 0), (0, 4), (3, 0)
+
+ab, bc, ac = distance(*point_a, *point_b), distance(*point_b, *point_c), distance(*point_a, *point_c)
+segments = ('AB', ab), ('AC', ac), ('BC', bc)
+
+answer, shortest_len = 'AB', ab
+
+for seg in segments:
+    if seg[1] < shortest_len:
+        answer=seg[0]
+
+
 print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
