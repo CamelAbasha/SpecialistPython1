04c21045eb5af35c4b788a7b71d518624b16efd2
CamelAbasha <100474748+CamelAbasha@users.noreply.github.com>
2022-03-24 22:22:34 +0300

    Update 03_task_except.py

--- Module7/practice/exceptions/03_task_except.py
+++ Module7/practice/exceptions/03_task_except.py
@@ -5,3 +5,20 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+
+
+line="2 12 -45 7 10"
+digits=line.split()
+positive=[]
+
+for d in digits:
+    try:
+        if int(d)>0:
+            positive.append(int(d))
+    except ValueError:
+        print(d,'- это не число')
+
+if len(positive)>0:
+    print(min(positive))
+else:
+    print('Положительных чисел в списке нет')
