08a1a123cc103df0ea65b338367686d27c5f0634
CamelAbasha <100474748+CamelAbasha@users.noreply.github.com>
2022-03-24 21:42:24 +0300

    Update 01_hw_file.py

--- Module6/home_work/01_hw_file.py
+++ Module6/home_work/01_hw_file.py
@@ -1,7 +1,13 @@
 # Напишите функцию log() принимающую в качестве аргумента строку и дописывающую это строку в конец файла
 
 def log(text, file="log.txt"):
-    pass
+    with open(file, "r", encoding="utf-8") as f:
+        lines = len(f.readlines())              #считает количество строк в файле
+    with open(file, "a", encoding="utf-8") as f:
+        if lines>0:
+            f.write('\n'+text)                  #если файл не пустой, то переносим строку и пишем текст
+        else:
+            f.write(text)                       #если файл пустой, то сразу пишем текст
 
 
 log("hello world")  # дописывает "hello world" в конец файла log.txt
